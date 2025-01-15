0b86bf1d1f977f79d6a64737910900ecf736a93c
bfaatime <fatimebexseliyeva@icloud.com>
2025-01-16 01:44:06 +0400

    added

---
+++ Home1/server/index.js
@@ -0,0 +1,23 @@
+
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const productRouter = require("./routes/productRoute");
+
+const DB_URL =
+  "mongodb+srv://fatimacbazmp202:fatima123@cluster0.b94na.mongodb.net/Shirt?retryWrites=true&w=majority&appName=Cluster0";
+const PASSWORD = "fatima123";
+const PORT = 8080;
+const app = express();
+app.use(cors());
+
+
+app.use(express.json());
+app.use("/api/products", productRouter);
+
+mongoose.connect(DB_URL).then(() => {
+  console.log("Connected!");
+  app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`);
+  });
+});
---
+++ Home1/server/models/productModel.js
@@ -0,0 +1,19 @@
+
+const mongoose = require("mongoose");
+
+// const { Schema } = mongoose;
+
+const Schema = mongoose.Schema;
+
+const ProductSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    price: { type: Number, required: true },
+    description: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+  }
+);
+
+const ProductModel = mongoose.model("Products", ProductSchema);
+
+module.exports = ProductModel;
